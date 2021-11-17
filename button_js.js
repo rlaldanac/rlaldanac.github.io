ed05d0d613bba57f9345dfa828d27bd7fc5948c3
Rodrigo <90422702+rlaldanac@users.noreply.github.com>
2021-11-17 09:07:49 -0500

    Add files via upload

---
+++ button_js.js
@@ -0,0 +1,20 @@
+console.log("javascript!!")
+
+const theButton = document.getElementById("Button");
+let theDiv = document.getElementById("vid");
+let the_video = document.createElement("iframe");
+
+theButton.addEventListener('click', rick_roll)
+
+function rick_roll(){
+
+    console.log("button clicked!!");
+    theDiv.appendChild(the_video)
+    the_video.setAttribute("src", "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    the_video.setAttribute("width", "560px")
+    the_video.setAttribute("height", "315px")
+    //theBody.style.backgroundColor = "pink";
+    //theBody.style.color = "white";
+    //thePara.textContent = "you pressed the button!!"
+    
+}
